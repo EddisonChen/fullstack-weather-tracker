d4e0a282afdf4210bae94cc69e6e3ce4470b9250
Eddison Chen <eddisonchen1@gmail.com>
2022-08-27 20:43:09 -0400

    created weather card

--- weather-tracker/src/containers/ShowWeather/ShowWeather.jsx
+++ weather-tracker/src/containers/ShowWeather/ShowWeather.jsx
@@ -1,8 +1,24 @@
 import "./ShowWeather.scss";
+import cityWeatherCard from "../../components/cityWeatherCard/cityWeatherCard";
 
 const ShowWeather = (props) => {
 
     const {weather} = props;
+
+        return (
+            <cityWeatherCard
+                cityName={weather.name}
+                countryName={weather.sys.country}
+                feelsLike={weather.main.feels_like}
+                humidity={weather.main.humidity}
+                temp={weather.main.temp}
+                maxTemp={weather.main.temp_max}
+                minTemp={weather.main.temp_min}
+                rain={weather.weather[0].description}
+                mainWeather={weather.weather[0].main}
+                wind={weather.wind.speed}
+                 />
+        )
 
 }
 
